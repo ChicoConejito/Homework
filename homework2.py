894affe3e5592c851bd975e107d2e1a80e4baf7e
ChicoConejito <cps-sirius@mail.ru>
2024-07-28 11:27:25 +0300

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+completed_tasks = '12,'
+completed_hours = '1.5,'
+course_name = 'Python,'
+time_for_task = 1.5 / 12
+print('Курс:',course_name,'всего задач:',completed_tasks,'затраченно часов',completed_hours,'среднее время выполнения',time_for_task,'часа.')
