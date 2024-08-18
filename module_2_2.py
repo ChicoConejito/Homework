c689f8275883f020d7b2b832e72dd2614dfc6b2d
ChicoConejito <cps-sirius@mail.ru>
2024-08-18 11:36:18 +0300

    Add files via upload

---
+++ module_2_2.py
@@ -0,0 +1,15 @@
+first = 22
+second = 69
+third = 112
+if first != second and second != third:
+    print(0)
+first = 22
+second = 69
+third = 69
+if first == second or second == third:
+    print(2)
+first = 69
+second = 69
+third = 69
+if first == second and second == third:
+    print(3)
